23e93a2d7cfc0c3d9eff4883d0175a9432db2fb1
gusleaooliveira <gus.leaono@gmail.com>
2020-05-23 18:27:05 -0300

    alteração feita em:23/05/2020-18:27:05

--- resources/js/escript.js
+++ resources/js/escript.js
@@ -5,4 +5,8 @@ $(document).ready(function(){
     $("#slideshow").append('<img src="resources/img/obra/imagem'+i+'.jpg">');
   }
 
+  for(let i = 0; i < 29; i++){
+    $("#gallery").append('<img src="resources/img/obra/imagem'+i+'.jpg">');
+  }
+
 });
