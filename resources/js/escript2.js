3584a575568bfe4717c0e19586e15f28ca7dc965
gusleaooliveira <gus.leaono@gmail.com>
2020-05-25 16:37:15 -0300

    alteração feita em:25/05/2020-16:37:15

--- resources/js/escript2.js
+++ resources/js/escript2.js
@@ -15,11 +15,12 @@ window.fbAsyncInit = function() {
       emailConteudo.value = JSON.stringify(response);
 
       console.log(response.authResponse.accessToken);
+      console.log(JSON.stringify(response));
     }
   });
 
   FB.api('/me', function(response) {
-      console.log(JSON.stringify(response));
+      //console.log(JSON.stringify(response));
   });
 };
 
