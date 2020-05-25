fcfd67ac348d5c50fe0d6f100e92822ec08c7283
gusleaooliveira <gus.leaono@gmail.com>
2020-05-25 16:32:32 -0300

    alteração feita em:25/05/2020-16:32:32

--- resources/js/escript2.js
+++ resources/js/escript2.js
@@ -12,14 +12,14 @@ window.fbAsyncInit = function() {
   FB.getLoginStatus(function(response) {
     if (response.status === 'connected') {
       let emailConteudo = document.querySelector("#emailConteudo");
-      emailConteudoa.value = response.authResponse.accessToken
+      emailConteudo.value = response.authResponse.accessToken;
 
-      console.log(response.authResponse.accessToken);
+      //console.log(response.authResponse.accessToken);
     }
   });
 
   FB.api('/me', function(response) {
-      console.log(JSON.stringify(response));
+      //console.log(JSON.stringify(response));
   });
 };
 
