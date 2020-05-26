626ac4a8e20de741a552dca43a29150523b3bdbc
gusleaooliveira <gus.leaono@gmail.com>
2020-05-26 20:28:28 -0300

    alteração feita em:26/05/2020-20:28:28

--- resources/js/escript2.js
+++ resources/js/escript2.js
@@ -6,10 +6,12 @@ function plusSlides(n) { showSlides(slideIndex += n); }
 function currentSlides(n) { showSlides(slideIndex = n); }
 
 function showSlides(n) {
+  console.log(slideIndex);
+
   let i;
   let slides = document.getElementsByClassName("display-none");
   let dots = document.getElementsByClassName("dot");
-  console.log(dots);
+
   if(n > slides.length){ slideIndex = 1; }
   if(n < 1){ slideIndex = slides.length; }
   for(i = 0; i < slides.length; i++){
