933754b9a871e69b4874da1fd5cafa83dcfc2b38
gusleaooliveira <gus.leaono@gmail.com>
2020-05-26 12:41:15 -0300

    alteração feita em:26/05/2020-12:41:15

--- resources/js/escript.js
+++ resources/js/escript.js
@@ -1,9 +1,19 @@
 
 $(document).ready(function(){
 
-  for(let i = 0; i < 5; i++){
-
-    $("#slideshow").append('<img src="resources/img/obra/imagem'+i+'.jpg" class="image-slideshow">');
+  let displaynone = "display-none";
+  let lenghtImages = 5;
+  for(let i = 0; i < lenghtImages; i++){
+    if(i == 0){displaynone = ""; }
+    else{displaynone = "display-none";}
+    $("#slideshow").append(
+      '<div class="'+displaynone+'">'+
+        '<p class="numbertext">'+i+'/'+lenghtImages+'</p>'+
+        '<img src="resources/img/obra/imagem'+i+'.jpg" class="image-slideshow ">'+
+        '<p class="caption-text">'+'Imagem de demonstração'+'</p>'+
+      '</div>'
+    );
+    $("#slideshowDots").append('<span class="dot" onclick="currentSlides('+i+')"></span>');
   }
 
   for(let i = 0; i < 29; i++){
