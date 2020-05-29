db65f5df6e4a89c7fc9559906d94d736e9659f42
gusleaono <gus.leaono@gmail.com>
2020-05-29 20:02:31 -0300

    alteração feita em:29/05/2020-20:02:31

--- salvar.sh
+++ salvar.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-
+git init
+# https://github.com/gusleaooliveira/MRConstrucoesApp.git
 git remote add origin git@github.com:gusleaooliveira/MRConstrucoesApp.git
 git add .
 git commit -m "alteração feita em:$(date +%d/%m/%Y-%k:%M:%S)"
